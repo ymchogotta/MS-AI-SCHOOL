259c3ff0d3c93f085f73788e9dd6dfcb5eed6694
YuminCho <cymerica20@gmail.com>
2022-12-15 14:30:19 +0900

    Update Training.js

--- JavaScript/4DAY/Training.js
+++ JavaScript/4DAY/Training.js
@@ -20,3 +20,37 @@
         }
     </script>
 </body>
+
+# p가 홀수이면 초록색, 짝수면 빨간색
+<body>
+    <p>4</p>
+    <script>
+        function isEven(value){
+            return value%2 == 0
+        }
+        let pElements = document.getElementsByTagName('p')
+        let result = isEven(parseInt(pElements[0].innerText))
+        if(result){
+            pElements[0].style.color = 'red'
+        }else{
+            pElements[0].style.color = 'green'
+        }
+    </script>
+</body>
+
+# p 중에서 홀수 번째인 것만 배경색을 red로
+<body>
+    <p>3</p>
+    <p>3</p>
+    <p>3</p>
+    <p>3</p>
+    <p>3</p>
+    <script>
+        let pElements = document.getElementsByTagName('p')
+        for(let i=0; i<pElements.length; i++){
+            if(i % 2 == 0){
+                pElements[i].style.backgroundColor = 'red'
+            }
+        }
+    </script>
+</body>
